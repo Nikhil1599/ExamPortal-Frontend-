c6d3285f5d048023dcd63879e6d8ff3d71e37475
Nikhilesh <nikhilsingh0745@gmail.com>
2023-05-11 09:34:11 +0530

    FA1

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit{
+  loginData = {
+    username:'',
+    password:''
+  }
+  constructor(private snack:MatSnackBar){}
+  ngOnInit(): void {
+
+  }
+  formSubmit(){
+    if(this.loginData.username.trim()==''|| this.loginData.username == null){
+      this.snack.open('Username is required !','',{duration:3000,});
+      return;
+    }
+    if(this.loginData.password.trim()==''|| this.loginData.password == null){
+      this.snack.open('Password is required !','',{duration:3000,});
+      return;
+    }
+  }
+}
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,45 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from './../../services/user.service';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  constructor(private userService:UserService, private snack:MatSnackBar) {}
+
+  public user = {
+    username: '',
+    password: '',
+    firstname: '',
+    lastname: '',
+    email: '',
+    phone: '',
+  };
+
+  ngOnInit(): void {}
+  formSubmit() {
+    if(this.user.username == '' || this.user.username == null){
+      this.snack.open("Username is required",'',{duration:3000,verticalPosition:'top',});
+      return;
+    }
+
+    //adduser: userservice
+    this.userService.addUser(this.user).subscribe(
+      (data:any)=>{
+        //success
+        console.log(data);
+        Swal.fire('Successfully Registered','User ID : '+data.id,'success');
+
+      },
+      (error)=>{
+        //error
+        console.log(error)
+        this.snack.open("Something went wrong",'',{duration:3000,verticalPosition:'top',});
+      }
+    )
+  }
+}
