24ff3347f0f3750eb4e27d8102aa6097cdb06c1f
Nikhilesh Singh <37766233+Nikhil1599@users.noreply.github.com>
2023-05-15 09:50:53 +0530

    F23

--- src/app/pages/admin/add-quiz/add-quiz.component.ts
+++ src/app/pages/admin/add-quiz/add-quiz.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CategoryService } from 'src/app/services/category.service';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-quiz',
@@ -9,13 +13,57 @@ export class AddQuizComponent implements OnInit {
 
   categories = [
     {
-      cid:19,
-      title:'Java'
+      cid: '',
+      title: '',
+      description: ''
     }
   ]
-  constructor(){}
-  ngOnInit(): void {
-
+  quizData = {
+    title: '',
+    description: '',
+    maxMarks: '',
+    numberOfQuestions: '',
+    active: true,
+    category: {
+      cid: ''
+    }
   }
 
+  constructor(private _cat: CategoryService, private _snack: MatSnackBar, private _quiz: QuizService) { }
+  ngOnInit(): void {
+    this._cat.categories().subscribe(
+      (data: any) => {
+        this.categories = data
+        console.log(this.categories)
+      },
+      (error) => {
+        console.log(error)
+        Swal.fire('Error!', 'Error while loading the data', 'error')
+      }
+    )
+  }
+  addQuiz() {
+    if (this.quizData.title.trim() == '' || this.quizData.title == null) {
+      this._snack.open("Title Required", '', { duration: 3000 });
+      return;
+    }
+    this._quiz.addQuiz(this.quizData).subscribe((data: any) => {
+      Swal.fire('Success', 'Exam added successfully', 'success')
+      this.quizData = {
+        title: '',
+        description: '',
+        maxMarks: '',
+        numberOfQuestions: '',
+        active: true,
+        category: {
+          cid: ''
+        }
+      }
+    },
+    (error)=>{
+      Swal.fire('Error','Error while adding the exam','error')
+      console.log(error)
+    }
+    )
+  }
 }
